44bbb872c1ffbc8d5707c71e3494b8a85274d94e
Brayan <40043757+Zero-NA@users.noreply.github.com>
2022-05-11 01:42:08 -0400

    Update Interface No Logic.py

--- Interface No Logic.py
+++ Interface No Logic.py
@@ -57,7 +57,7 @@ password.grid(row=0, column=1)
 btn_generate = Button(Form, text="Check", width=20, command= checkPassword)
 btn_generate.grid(row=1, columnspan=2)
 #====================================OUTPUT WIDGET==============================
-password = Entry(Form, textvariable=OUTPUT, font=(18), width=16)
+password = Entry(Form, textvariable=password, font=(18), width=16)
 password.grid(row=5, column=5)
 
 root.mainloop()
