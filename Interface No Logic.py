96915c22881e14aa17579782fa28f83ed0ba0d7d
Brayan <Brayanvillatoro90@gmail.com>
2022-05-11 17:02:38 -0400

    Update Interface No Logic.py

--- Interface No Logic.py
+++ Interface No Logic.py
@@ -5,7 +5,7 @@ import requests
 
 root = Tk()
 
-root.title("EASY PASS")
+root.title("CSCI 401 Machine Learning Group: Brayan | Steven | Tamara")
 width = 500
 height = 600
 screen_width = root.winfo_screenwidth()
@@ -34,7 +34,7 @@ def checkPassword():
         password_dict[hash[0]] = hash[1]
     # Output to program display
     if sha_post in password_dict.keys():
-       lbl_title = Label(Bottom, width=width, font=('arial', 16), fg = 'red',text='Password has been breached {0}'.format(password_dict[sha_post]), bd=1, relief='flat')
+       lbl_title = Label(Bottom, width=width, font=('arial', 16), fg = 'red',text='Password has been breached {0} times'.format(password_dict[sha_post]), bd=1, relief='flat')
        lbl_title.pack(fill=X)
     else:
         lbl_title = Label(Bottom, width=width, font=('arial', 16), fg = 'green',text="No breaches detected!", bd=1, relief='flat')
@@ -54,9 +54,9 @@ Bottom = Frame(root, width=width)
 Bottom.pack(side=TOP)
 
 # LABEL WIDGET
-lbl_title = Label(Top, width=width, font=('arial', 21), text="Machine Learning Project", bd=1, relief='flat')
+lbl_title = Label(Top, width=width, font=('Arial Black', 45),fg= 'Blue', text="Easy Pass", bd=1, relief='flat')
 lbl_title.pack(fill=X)
-lbl_password = Label(Form, font=('arial', 14), text="Enter Password ", bd=11)
+lbl_password = Label(Form, font=('Arial', 14), text="Enter Password ", bd=11)
 lbl_password.grid(row=0, pady=17)
 
 # ENTRY WIDGET
@@ -64,7 +64,7 @@ password = Entry(Form, textvariable=PASSWORD, font=(18), width=16)
 password.grid(row=0, column=1)
 
 # BUTTON WIDGET
-btn_generate = Button(Form, font=('arial',11),text="Check", width=20, command= checkPassword)
+btn_generate = Button(Form, font=('Arial',11),text="Check", width=20, command= checkPassword)
 btn_generate.grid(row=3, columnspan=2)
 
 root.mainloop()
